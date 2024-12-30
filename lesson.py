8fc43fe4d25b79cd872b20d83ec1f2d5874a77ca
aleuxgo <madalex-nb@yandex.ru>
2024-12-30 12:22:26 +0400

    add 4 files

---
+++ lesson.py
@@ -0,0 +1,10 @@
+def fit_in_boxes(content, slot=2):
+    sorted_into_boxes = [content[i:i + slot] for i in range(0, len(content), slot)]
+    # sorted_into_boxes = [box for box in sorted_into_boxes if box]
+
+    # if slot <= 0:
+    # raise ValueError('Размер подсписка должен быть положительным целым числом')
+    return sorted_into_boxes
+
+
+print(fit_in_boxes([1, 2, 3, 4, 5, 6, 7, 8, 9], 4))
